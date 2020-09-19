3640603a74a72e91e6f01d79fe8b2b617ab2e55e
nimra <nimra@DESKTOP-1TV34D9.myfiosgateway.com>
2020-09-18 22:13:03 -0400

    week2 Assigment EmployeeInfo

---
+++ EmployeProject/src/EmployeProject/Employee.java
@@ -0,0 +1,15 @@
+package EmployeProject;
+
+public class Employee {
+	int EmployeeId;
+	String EmployeeName;
+	
+	public Employee(int employeeid, String employeeName) {
+		this.EmployeeId = employeeid;
+		this.EmployeeName = employeeName;
+
+	
+	
+	}
+
+}
---
+++ EmployeProject/src/EmployeProject/EmployeeInfo.java
@@ -0,0 +1,12 @@
+package EmployeProject;
+
+public class EmployeeInfo {
+
+	public static void main(String[] args) {
+		Employee sania = new Employee(20,"sania");
+		System.out.println(sania.EmployeeId + " "  + sania.EmployeeName);
+		
+
+	}
+
+}
